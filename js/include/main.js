4df843bf11fe698ca5a6ef1023dbff2afac74759
DillanPie <dillansuon@gmail.com>
2026-02-20 12:15:46 -0500

    Refactor to use Vite Builder to speed loading times

--- js/include/main.js
+++ js/include/main.js
@@ -8,7 +8,7 @@ import '/css/footer.css';
 
 // === Import Global JavaScript ===
 // Will be bundled into the main JS file.
-import '/js/navbar.js';
-import '/js/footer.js';
+import './navbar.js';
+import './footer.js';
 
 console.log("Vite main entry point loaded.");
---
+++ js/include/resources-page.js
@@ -0,0 +1,18 @@
+// js/resources-page.js
+
+// === IMPORT GLOBAL ASSETS FIRST ===
+// These are the base styles and scripts for all pages.
+import '../../css/navbar.css';
+import '../../css/footer.css';
+import '../../css/style.css';
+import '../../css/resources.css';
+
+
+import './navbar.js';
+import './footer.js';
+
+// === IMPORT PAGE-SPECIFIC ASSETS LAST ===
+// This ensures its styles will override the global styles.
+ 
+
+console.log("Resources page assets loaded.");
