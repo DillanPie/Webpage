1124a45cb3287c0d185a203a80c5e3c1af4bf6ea
DillanPie <dillansuon@gmail.com>
2026-03-23 10:16:41 -0400

    Converted startpage to use the Vite way for constructing the page

---
+++ js/include/pages/start-page.js
@@ -0,0 +1,13 @@
+// js/include/pages/about-page.js
+
+// Step 1: Import the global assets (like navbar.css, footer.css, etc.)
+import '../main.js';
+
+// Step 2: Import the page-specific assets for the homepage.
+
+import '../../../css/startpage.css';
+
+// Import the slideshow's JavaScript logic
+import '../../app.js';
+
+console.log("Start Page(start-page.js) assets have been loaded.");
